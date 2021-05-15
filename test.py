a47b5b5d2d8e23579dbe21798daa3b48bd8da283
Nikita Breskanu <50447080+Fullfix@users.noreply.github.com>
2021-05-15 20:20:28 +0400

    Add docker

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 from mavlink.Mavlink import Mavlink
 from mavlink.DronepointController import DronepointController
 import socket
+from pymavlink import mavutil, mavwp
 
 def main():
     # mavlink = Mavlink()
     # mavlink.test_command()
-    dp = DronepointController()
-    dp.main()
+    # dp = DronepointController()
+    # dp.main()
+    url = 'udpout:192.168.194.9:14590'
+    mavconn = mavutil.mavlink_connection(url, source_system=255)
+
 
 if __name__ == '__main__':
     main()
